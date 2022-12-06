3d7b6fbea52a957ade52f4d2f1a26dc61d9abcfb
Katerina <m-a-c-u-k-a@mail.ru>
2022-12-07 02:47:28 +0700

    удаляем строку и столбец массива

---
+++ Задача4/Program.cs
@@ -0,0 +1,106 @@
+﻿/* Задайте двумерный массив из целых чисел. Напишите программу, которая удалит строку и столбец, на пересечении которых расположен наименьший элемент массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Наименьший элемент - 1, на выходе получим 
+следующий массив:
+9 4 2
+2 2 6
+3 4 7 */
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void FillMatrixRandomNumbers(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0,10);
+        }
+    }
+}
+
+void WriteMatrix(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int Min(int[,]array)
+{
+    int min =int.MaxValue;
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i,j]<min)
+            {
+                min = array[i,j];
+            }
+        }
+    }
+    return min;
+}
+
+int [] Find(int[,]array)
+{
+    int b = Min(array);
+    int [] m =new int[2];
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i,j]==b)
+            {
+                m[0]=i;
+                m[1]=j;
+                break;
+            }
+        }
+    }
+    return m;
+}
+
+int [,] DeletedMas(int [,]array)
+{
+    int [] c=Find(array);
+    int [,] otv=new int [array.GetLength(0),array.GetLength(1)];
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            
+            if (i!=c[0]&&j!=c[1])
+            {
+
+                otv[i,j]=array[i,j];
+                Console.Write(otv[i,j]+" ");
+            }
+        
+        }
+        Console.WriteLine();
+    }
+    return otv;
+}
+
+int rows = ReadInt("Введите количество строк: ");
+int columns = ReadInt("Введите количество столбцов: ");
+int[,] numbers = new int[rows, columns];
+
+FillMatrixRandomNumbers(numbers);
+WriteMatrix(numbers);
+WriteMatrix(DeletedMas(numbers));
